4b93444421d9a9327ad40e3f56c712617c358db4
Andy Christianson <nix@nixxed.com>
2014-10-11 22:47:28 -0500

    Add graceful shutdown code to server.js.

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, '../build')));
 
 // catch 404 and forward to error handler
-app.use(function (req, res) {
+app.use(function(req, res) {
     res.writeHead(404);
     res.end('404: Not found');
 });
@@ -22,8 +22,23 @@ app.use(function (req, res) {
 //start the server
 var port = process.env.PORT || 3000;
 app.set('port', port);
-app.listen(port, function () {
+var server = app.listen(port, function() {
     console.log('Express server listening on port ' + port);
+});
+
+//stop the server gracefully on kill signal
+process.on('SIGTERM', function() {
+    server.close(function() {
+        console.log('Received kill signal, shutting down gracefully...');
+        //close out any db/etc stuff here
+        process.exit(1);
+    });
+
+    setTimeout(function() {
+        console.error('Could not shut down gracefully in time, forcefully shutting down...');
+        process.exit(1);
+    }, 30 * 1000);
+
 });
 
 module.exports = app;
