495f523e923a04c3953a058f3d30f15084b19945
Andy Christianson <nix@nixxed.com>
2014-10-21 10:19:08 -0500

    Enhance build process to utilize livereload for faster page refresh while in debug.

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,15 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var ngAnnotate = require('gulp-ng-annotate');
+var changed = require('gulp-changed');
+var gulpif = require('gulp-if');
+var livereload = require('gulp-livereload');
+var injectReload = require('gulp-inject-reload');
 
 var paths = {
-    js: [
-        'node_modules/jquery/dist/jquery.js',
+    base: 'client',
+    target: 'build',
+    js: ['node_modules/jquery/dist/jquery.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
         'node_modules/angular/angular.js',
         'node_modules/angular-route/angular-route.js',
@@ -18,8 +23,8 @@ var paths = {
         'client/**/*.js'],
     css: ['client/style.css',
         'client/vendor/css/bootstrap.slate.css'],
-    html: ['client/index.html',
-        'client/**/*.html']
+    index_html: 'client/index.html',
+    html: ['client/**/*.html', '!client/index.html']
 };
 
 //clean up old build
@@ -27,39 +32,58 @@ gulp.task('clean', function clean(callback) {
     del(['build'], callback);
 });
 
+//copy over index.html with livereload if in debug
+gulp.task('build-index', buildIndex(false));
+gulp.task('build-index-livereload', buildIndex(true));
+function buildIndex(debug) {
+    return function() {
+        gulp.src(paths.index_html, { base: paths.base })
+            .pipe(gulpif(debug, injectReload()))
+            .pipe(changed(paths.target, {hasChanged: changed.compareSha1Digest}))
+            .pipe(gulp.dest(paths.target));
+    }
+}
+
 //copy over html
-gulp.task('html', function() {
-    gulp.src(paths.html, { base: 'client' })
+gulp.task('build-html', function() {
+    gulp.src(paths.html, { base: paths.base })
         .pipe(clip())
-        .pipe(gulp.dest('build'));
+        .pipe(changed(paths.target, {hasChanged: changed.compareSha1Digest}))
+        .pipe(gulp.dest(paths.target));
 });
 
 //minify and concat all js
-gulp.task('minify-js', function() {
+gulp.task('build-js', function() {
     gulp.src(paths.js)
         .pipe(clip())
         .pipe(sourcemaps.init())
         .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(concat('all.min.js'))
+        .pipe(changed(paths.target, {hasChanged: changed.compareSha1Digest}))
         .pipe(sourcemaps.write('.', {addComment: false}))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(paths.target));
 });
 
 //minify and concat all css
-gulp.task('minify-css', function() {
+gulp.task('build-css', function() {
     gulp.src(paths.css)
         .pipe(clip())
-        .pipe(minifyCSS({keepBreaks:true}))
+        .pipe(minifyCSS({keepBreaks: true}))
         .pipe(concat('all.min.css'))
-        .pipe(gulp.dest('build'));
+        .pipe(changed(paths.target, {hasChanged: changed.compareSha1Digest}))
+        .pipe(gulp.dest(paths.target));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch(paths.html, ['html']);
-    gulp.watch(paths.js, ['minify-js']);
-    gulp.watch(paths.css, ['minify-css']);
+    gulp.watch(paths.index_html, ['build-index-livereload']);
+    gulp.watch(paths.html, ['build-html']);
+    gulp.watch(paths.js, ['build-js']);
+    gulp.watch(paths.css, ['build-css']);
+
+    livereload.listen();
+    gulp.watch(paths.target + '/**').on('change', livereload.changed);
 });
 
 gulp.task('server', function() {
@@ -68,6 +92,5 @@ gulp.task('server', function() {
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['server', 'html', 'minify-js', 'minify-css']);
-gulp.task('debug', ['server', 'watch', 'html', 'minify-js', 'minify-css']);
-gulp.task('build', ['html', 'minify-js', 'minify-css']);
+gulp.task('default', ['server', 'watch', 'build-index-livereload', 'build-html', 'build-js', 'build-css']);
+gulp.task('build', ['build-index', 'build-html', 'build-js', 'build-css']);
